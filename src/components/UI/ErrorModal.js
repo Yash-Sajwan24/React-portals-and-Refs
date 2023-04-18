f54a202c86c23e5f9c76a472f87af0badd340709
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-04-19 02:51:39 +0530

    portals

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
+
+const BackDrop = (props) => {
+  return (
+    <>
+      <div className={classes.backdrop} onClick={props.onConfirm} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    </>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <>
+      {ReactDom.createPortal(
+        <BackDrop onConfirm={props.onConfirm} title={props.title} message={props.message} />,
+        document.getElementById("errorModal")
+      )}
+    </>
+  );
+};
+
+export default ErrorModal;
